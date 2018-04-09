15f9b620ca35651f6f50e44a234a3e249fd69c26
julianec93 <37894914+julianec93@users.noreply.github.com>
2018-04-09 22:51:25 +0200

    Update simon_says.rb

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -10,6 +10,10 @@ def repeat (string, n = 2)
        [string] * n * ' '
 end
 
-def start_of_word (a)
-  String lettre = a.subString(1);
-end
+  def start_of_word(string, n = 0)
+      return string[0...n]
+   end
+
+   def first_word (string, n = 0 )
+        string.split.first
+   end
